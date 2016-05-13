07df24829af59a0ed547c43f221c44760d180acb
artsiomv <artsiomv@uw.edu>
2016-05-13 13:57:37 -0700

    changed from CBC to CTR, added HMAC

--- AES.java
+++ AES.java
@@ -9,23 +9,19 @@ import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
-public class AES 
-{   
-    public static byte[] encrypt(final byte[] keyBytes, final byte[] ivBytes, final byte[] messageBytes) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException
-    {       
+public class AES {   
+    public static byte[] encrypt(final byte[] keyBytes, final byte[] ivBytes, final byte[] messageBytes) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException{       
         return AES.transform(Cipher.ENCRYPT_MODE, keyBytes, ivBytes, messageBytes);
     }
 
-    public static byte[] decrypt(final byte[] keyBytes, final byte[] ivBytes, final byte[] messageBytes) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException
-    {       
+    public static byte[] decrypt(final byte[] keyBytes, final byte[] ivBytes, final byte[] messageBytes) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException{       
         return AES.transform(Cipher.DECRYPT_MODE, keyBytes, ivBytes, messageBytes);
     }
 
-    private static byte[] transform(final int mode, final byte[] keyBytes, final byte[] ivBytes, final byte[] messageBytes) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException
-    {
+    private static byte[] transform(final int mode, final byte[] keyBytes, final byte[] ivBytes, final byte[] messageBytes) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
         final SecretKeySpec keySpec = new SecretKeySpec(keyBytes, "AES");
         final IvParameterSpec ivSpec = new IvParameterSpec(ivBytes);
-        final Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        final Cipher cipher = Cipher.getInstance("AES/CTR/PKCS5Padding");
 
         cipher.init(mode, keySpec, ivSpec);
 
--- main.java
+++ main.java
@@ -7,15 +7,18 @@ import java.io.UnsupportedEncodingException;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.security.SignatureException;
 import java.util.Base64;
 
+import org.apache.commons.*;
 import javax.crypto.BadPaddingException;
-import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.Mac;
 import javax.crypto.NoSuchPaddingException;
-import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import javax.xml.bind.DatatypeConverter;
+
+import org.omg.IOP.Encoding;
 
 public class main {
 
@@ -24,26 +27,48 @@ public class main {
 	public static void main(String[] args) {
 		try {
 			final String base64Key = "ABEiM0RVZneImaq7zN3u/w==";
+			final String base64KeyHMAC = "PSgiM6BVZneImoq7zY3T/w==";
 		    final String base64Iv = "AAECAwQFBgcICQoLDA0ODw==";
 		    final byte[] keyBytes = DatatypeConverter.parseBase64Binary(base64Key);
 		    final byte[] ivBytes = DatatypeConverter.parseBase64Binary(base64Iv);
 		    
-		    //Want to modify encoded txt? outcomment these lines
 			readFile();
+			
 		    String base64Message = DatatypeConverter.printBase64Binary(sb.toString().getBytes());
 		    final byte[] messageBytes = DatatypeConverter.parseBase64Binary(base64Message);
 		    final byte[] encryptedBytes = AES.encrypt(keyBytes, ivBytes, messageBytes);
+		    
+		    //CREATE HMAC
+		    String writeHMAC = calculateHMAC(new String(encryptedBytes, "UTF-8"), base64KeyHMAC);
+		    System.out.println(writeHMAC);
+		    
+		    
+		    /***************
+		     * REMOVE THIS LINE TO NOT CHANGE THE FILE
+		     */
+//		    encryptedBytes[5] = 'i';		    
+		    /***************
+		     * REMOVE THIS LINE TO NOT CHANGE THE FILE
+		     */
 		    writeToFile(encryptedBytes);
-		    ///////////////////
 		    
 		    readFromFile();
-			
-		    final byte[] decryptedBytes = AES.decrypt(keyBytes, ivBytes, DatatypeConverter.parseBase64Binary(sb.toString()));//encryptedBytes);        
+		    //GET HMAC
+		    String endHMAC = calculateHMAC(new String(DatatypeConverter.parseBase64Binary(sb.toString()), "UTF-8"), base64KeyHMAC);
+		    System.out.println(endHMAC);
+		    
 
-		    System.out.println("Message  : " + new String(messageBytes, "UTF-8"));
-		    System.out.println("Encrypted: " + DatatypeConverter.printBase64Binary(encryptedBytes));
-		    System.out.println("Decrypted: " + new String(decryptedBytes, "UTF-8"));
-            
+		    //COMPARE HMAC
+		    if(writeHMAC.equals(endHMAC)) {
+		    	final byte[] decryptedBytes = AES.decrypt(keyBytes, ivBytes, DatatypeConverter.parseBase64Binary(sb.toString()));
+		    	System.out.println("HMAC CORRECT");
+//		    	System.out.print(new String(messageBytes, "UTF-8"));
+//		    	System.out.println("Encrypted: " + DatatypeConverter.printBase64Binary(encryptedBytes));
+//		    	System.out.print(new String(decryptedBytes, "UTF-8"));
+		    } else {
+		    	System.out.println("HMAC incorrect, the file was changed");
+		    }
+		    
             
 		} catch (FileNotFoundException e1) {
 			e1.printStackTrace();
@@ -61,7 +86,24 @@ public class main {
 			e.printStackTrace();
 		} catch (BadPaddingException e) {
 			e.printStackTrace();
+		} catch (SignatureException e) {
+			e.printStackTrace();
 		}
+	}
+	
+	private static final String HMAC_SHA1_ALGORITHM = "HmacSHA1";
+	public static String calculateHMAC(String data, String key) throws java.security.SignatureException {
+		String result;
+		try {
+			SecretKeySpec signingKey = new SecretKeySpec(key.getBytes(), HMAC_SHA1_ALGORITHM);
+			Mac mac = Mac.getInstance(HMAC_SHA1_ALGORITHM);
+			mac.init(signingKey);
+			byte[] rawHmac = mac.doFinal(data.getBytes());
+			result = Base64.getEncoder().encodeToString(rawHmac);
+		} catch (Exception e) {
+			throw new SignatureException("Failed to generate HMAC : " + e.getMessage());
+		}
+		return result;
 	}
 	
 	public static void readFile() throws IOException {
@@ -90,7 +132,6 @@ public class main {
 		
 		while (line != null) {
 			sb.append(line);
-			sb.append("\n");
 			line = br.readLine();
 		}
 		br.close();
